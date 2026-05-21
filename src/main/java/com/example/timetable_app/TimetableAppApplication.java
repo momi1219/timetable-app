7d191949682fc750fa8618a50b16e97c69fb7ea5
momi1219 <mmymyt1219@gmail.com>
2026-05-21 22:53:44 +0900

    Initial commit

---
+++ src/main/java/com/example/timetable_app/TimetableAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.timetable_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timetable_app/TimetableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timetable_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
